b25639d7f8c504e9576aa3ce743f2c08c7eb4396
Amos Rossman <amosrossman@gmail.com>
2022-08-30 15:34:48 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -24,7 +24,7 @@ export { HeroDark as Dark };
 export const githubUsername = "AmosRM";
 
 // Blog link icon (imported above)
-export const Blog = <FaLink />;
+export const Blog = <FaLinkedin />;
 
 
 /* About Me
